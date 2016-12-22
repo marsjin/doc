9ff7a3208534db436d98afba2ca80e6e0f70d30e
marsjin <marsjin@sina.com>
2016-12-22 09:42:21 +0800

    Update ps.sh

--- ps.sh
+++ ps.sh
@@ -1,3 +1,8 @@
 1、查询进程cpu使用率排序
 ps H -eo user,pid,ppid,tid,time,%cpu,cmd --sort=%cpu
 
+2、linux 查看进程启动时间（运行时间）
+ps -eo lstart 启动时间 
+ps -eo etime 运行多长时间.
+ps -eo pid,lstart,etime,args|grep -v grep| grep -E 'node|PID' --color
+
